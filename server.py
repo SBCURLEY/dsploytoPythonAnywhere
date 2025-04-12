ebbcee0d35ed803d5f9e9c3c7170a7d61bf55e46
Sharon Curley <sbrogancurley@gmail.com>
2025-04-12 19:00:39 +0100

    Hello Sharons World

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return "Hello world"
+        return "Hello Sharons world"
 
 if __name__ == "__main__":
         app.run(debug = True)
